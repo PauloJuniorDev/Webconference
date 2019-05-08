954b9cdedc8038ceb699eca1643075125ecc34ff
Paulo Correia <pfcorreia91@gmail.com>
2019-05-08 17:21:05 +0100

    removed Back-end

--- ParteIII_Back-end/workspace/config/connectMySQL.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-module.exports = {
-	con: mysql.createConnection({
-		host     : 'webitcloud.net',
-		user     : 'webitclo_webbook',
-		password : 'webbookPW#2018',
-		database : 'webitclo_webbook'
-	})
-};
